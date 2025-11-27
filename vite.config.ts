e06bfd7fac82cfc2dc1601be83b02220a43e05b8
wisinth <155398626+wisinth@users.noreply.github.com>
2025-11-28 00:40:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  ssr: {
-    noExternal: true,
-  },
   build: {
-    ssr: false,
+    outDir: "dist",
   },
 });
