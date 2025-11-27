da4d7e1a34070cffea1de8bf0f1712d9b857ebfc
wisinth <155398626+wisinth@users.noreply.github.com>
2025-11-27 21:53:07 +0700

    update title text

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -31,7 +31,7 @@ function App() {
           />
         </a>
       </div>
-      <h1>Vite + React + Hono + 123456789</h1>
+      <h1>Test Edit This Page</h1>
       <div className="card">
         <button
           onClick={() => setCount((count) => count + 1)}
